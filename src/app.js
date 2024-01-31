76e351f2f0b1c2a3f386d026db4218a9081072c6
Larssies <larsvanwylick@gmail.com>
2024-01-31 12:54:47 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,28 +1,66 @@
+const https = require('https');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8443;
 
-const picturesFolder = path.join(__dirname, 'pictures');
+const currentTime = new Date();
+const hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const formattedHours = hours < 10 ? `0${hours}` : hours;
+const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+const currentTimeString = `${formattedHours}:${formattedMinutes}`;
+
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.larssies.site/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.larssies.site/fullchain.pem'),
+};
+
+const server = https.createServer(options, app);
+
+const picturesFolder = path.join(__dirname, 'cdn');
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/random', (req, res) => {
     fs.readdir(picturesFolder, (err, files) => {
         if (err) {
             console.error('Error reading pictures folder:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
+            return res.status(500).json({
+                error: 'Internal Server Error',
+                success: false 
+            });
         }
 
         const randomIndex = Math.floor(Math.random() * files.length);
-        const randomImage = path.join('/pictures', files[randomIndex]).replace(/\\/g, '/');
-        const fullUrl = `${req.protocol}://${req.get('host')}${randomImage}`;
+        const randomImage = path.join('/cdn', files[randomIndex]).replace(/\\/g, '/');
+        const fullUrl = `https://${req.get('host')}${randomImage}`;
 
-        res.json({ url: fullUrl });
+        console.log(`[${currentTimeString}] Request received for ${req.url}`);
+
+        res.json({
+            url: fullUrl, 
+            success: true
+        });
     });
 });
 
-app.use('/pictures', express.static('pictures'));
+app.use('/cdn', express.static('cdn'));
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Server is running at https://api.larssies.site/random`);
+});
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Server is gracefully shutting down');
+        process.exit(0);
+    });
 });
--- test/app.js
+++ test/app.js
@@ -6,18 +6,32 @@ const port = 3000;
 
 const picturesFolder = path.join(__dirname, 'pictures');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
+
 app.get('/random', (req, res) => {
     fs.readdir(picturesFolder, (err, files) => {
         if (err) {
             console.error('Error reading pictures folder:', err);
-            return res.status(500).json({ error: 'Internal Server Error' });
+            return res.status(500).json({
+                error: 'Internal Server Error',
+                success: false 
+            });
         }
 
         const randomIndex = Math.floor(Math.random() * files.length);
         const randomImage = path.join('/pictures', files[randomIndex]).replace(/\\/g, '/');
         const fullUrl = `${req.protocol}://${req.get('host')}${randomImage}`;
 
-        res.json({ url: fullUrl });
+        res.json({
+            url: fullUrl, 
+            success: true
+        });
     });
 });
 
